5849516ef291ef266ac01774966fe15b91da3c0f
srivatsavd2 <136217897+srivatsavd2@users.noreply.github.com>
2024-09-13 10:01:37 +0530

    Update viewer.js

--- public/viewer.js
+++ public/viewer.js
@@ -1,1 +1,41 @@
+window.onload = () => {
+    document.getElementById('my-button').onclick = () => {
+        init();
+    }
+}
 
+async function init() {
+    const peer = createPeer();
+    peer.addTransceiver("video", { direction: "recvonly" });
+}
+
+function createPeer() {
+    const peer = new RTCPeerConnection({
+        iceServers: [
+            { urls: "stun:stun.stunprotocol.org" }
+        ]
+    });
+    peer.ontrack = handleTrackEvent;
+    peer.onnegotiationneeded = () => handleNegotiationNeededEvent(peer);
+    return peer;
+}
+
+async function handleNegotiationNeededEvent(peer) {
+    const offer = await peer.createOffer();
+    await peer.setLocalDescription(offer);
+    const payload = {
+        sdp: peer.localDescription
+    };
+
+    try {
+        const { data } = await axios.post('https://livestream-bchc.onrender.com/consumer', payload);
+        const desc = new RTCSessionDescription(data.sdp);
+        await peer.setRemoteDescription(desc);
+    } catch (error) {
+        console.error("Error in handling negotiation needed event:", error);
+    }
+}
+
+function handleTrackEvent(e) {
+    document.getElementById("video").srcObject = e.streams[0];
+}
